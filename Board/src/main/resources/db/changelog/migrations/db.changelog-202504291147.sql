4e4d4b7423a31f573d1189dc959f347a3910d205
Thaize Paulo <thaizepaulo@gmail.com>
2025-04-29 16:14:09 -0300

    correção dos arquivos de migração

--- Board/src/main/resources/db/changelog/migrations/db.changelog-202504291147.sql
+++ Board/src/main/resources/db/changelog/migrations/db.changelog-202504291147.sql
@@ -7,7 +7,7 @@ CREATE TABLE BLOCKS(
     blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     block_reason VARCHAR(255) NOT NULL,
     unblocked_at TIMESTAMP NULL,
-    unblock_reason VARCHAR(255) NOT NULL,
+    unblock_reason VARCHAR(255) NULL,
     card_id BIGINT NOT NULL,
     CONSTRAINT cards__blocks_fk FOREIGN KEY (card_id) REFERENCES CARDS(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
