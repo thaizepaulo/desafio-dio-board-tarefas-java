67519b0c8b49b4c855ef283f5c08ae6dc165a29b
Thaize Paulo <thaizepaulo@gmail.com>
2025-04-29 16:42:50 -0300

    Criação do BoardQueryService com os metodos findById

---
+++ Board/src/main/java/br/com/dio/service/BoardQueryService.java
@@ -0,0 +1,26 @@
+package br.com.dio.service;
+
+import br.com.dio.persistence.dao.BoardDAO;
+import br.com.dio.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardQueryService {
+
+    private final Connection connection;
+
+    public Optional<BoardEntity> findById(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        var optional = dao.findById(id);
+        if (optional.isPresent()){
+            var entity = optional.get();
+            return Optional.of(entity);
+        }
+        return Optional.empty();
+    }
+
+}
