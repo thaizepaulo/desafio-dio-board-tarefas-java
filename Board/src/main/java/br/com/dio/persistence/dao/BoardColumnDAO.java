1c5b942ec0e6910be04f789775dd9a00d43677fe
Thaize Paulo <thaizepaulo@gmail.com>
2025-04-29 17:03:55 -0300

    Criação do metodo findByBoardId no BoardColumnDAO

--- Board/src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ Board/src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -6,6 +6,10 @@ import lombok.RequiredArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static br.com.dio.persistence.entity.BoardColumnKindEnum.findByName;
 
 @RequiredArgsConstructor
 public class BoardColumnDAO {
@@ -28,5 +32,23 @@ public class BoardColumnDAO {
         }
     }
 
+    public List<BoardColumnEntity> findByBoardId(final Long boardId) throws SQLException{
+        List<BoardColumnEntity> entities = new ArrayList<>();
+        var sql = "SELECT id, name, `order`, kind FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY `order`";
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, boardId);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while (resultSet.next()){
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setKind(findByName(resultSet.getString("kind")));
+                entities.add(entity);
+            }
+            return entities;
+        }
+    }
 
 }
