93ac28b43262262ff47e19713dee74c2bbf74d75
Thaize Paulo <thaizepaulo@gmail.com>
2025-04-30 10:57:58 -0300

    Detalhar colunas ao pesquisar board por id na classe BoardQueryService

--- Board/src/main/java/br/com/dio/service/BoardQueryService.java
+++ Board/src/main/java/br/com/dio/service/BoardQueryService.java
@@ -1,5 +1,6 @@
 package br.com.dio.service;
 
+import br.com.dio.persistence.dao.BoardColumnDAO;
 import br.com.dio.persistence.dao.BoardDAO;
 import br.com.dio.persistence.entity.BoardEntity;
 import lombok.AllArgsConstructor;
@@ -15,9 +16,11 @@ public class BoardQueryService {
 
     public Optional<BoardEntity> findById(final Long id) throws SQLException {
         var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
         var optional = dao.findById(id);
         if (optional.isPresent()){
             var entity = optional.get();
+            entity.setBoardColumns(boardColumnDAO.findByBoardId(entity.getId()));
             return Optional.of(entity);
         }
         return Optional.empty();
